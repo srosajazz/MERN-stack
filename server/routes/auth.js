d9e1f3dbf8cab1318dc8d404e8211da917d5e7cb
Sergio Rosa <scarlosjazz@gmail.com>
2020-03-29 16:11:20 -0400

    Express validator

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-//import controller
-const { signup } = require('../controllers/auth')
+// import controller
+const { signup } = require('../controllers/auth');
 
-router.get('/signup', signup);
+// import validators
+const { userSignupValidator } = require('../validators/auth');
+const { runValidation } = require('../validators');
+
+router.post('/signup', userSignupValidator, runValidation, signup);
 
 module.exports = router;
